ec64f3f3e8470e8e2ec60a111fe95a73f0bec38b
cc23158 - Victor <142424989+cc23158@users.noreply.github.com>
2024-08-15 08:55:35 -0300

    ok

--- SF/src/main/java/com/example/SF/DAL/ClientController.java
+++ SF/src/main/java/com/example/SF/DAL/ClientController.java
@@ -2,10 +2,7 @@ package com.example.SF.DAL;
 
 import com.example.SF.BLL.ClientService;
 import com.example.SF.DTO.Client;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,7 +22,7 @@ public class ClientController {
     }
 
     @GetMapping("/getClientByName")
-    public Client getByName(@RequestParam String name){
+    public Client getByName(@PathVariable String name){
         return clientService.getByName(name);
     }
 
