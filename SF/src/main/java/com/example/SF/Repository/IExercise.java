f1ea9e6d8d97441a0aa7edf4ca19979c51db21b1
VictorYJM <142424989+cc23158@users.noreply.github.com>
2024-10-03 11:15:21 -0300

    a

--- SF/src/main/java/com/example/SF/Repository/IExercise.java
+++ SF/src/main/java/com/example/SF/Repository/IExercise.java
@@ -13,34 +13,6 @@ public interface IExercise extends JpaRepository<Exercise, Integer> {
     @Query(value = "SELECT * FROM SF.GET_Exercise(:muscleId)", nativeQuery = true)
     List<Exercise> getByMuscle(@Param("muscleId") Integer id);
 
-    @Query(value = "SELECT * FROM SF.Exercise WHERE exercise_image = :exerciseImage AND exercise_active = TRUE", nativeQuery = true)
+    @Query(value = "SELECT * FROM SF.Exercise WHERE exercise_image = :exerciseImage", nativeQuery = true)
     List<Exercise> findByImage(@Param("exerciseImage") String image);
-
-    @Modifying
-    @Query("UPDATE Exercise SET exercise_name = :name, exercise_image = :image, exercise_path = :path, exercise_muscle = :muscleId WHERE exercise_id = :id")
-    void updateExercise(
-            @Param("id") Integer id,
-            @Param("name") String name,
-            @Param("image") String image,
-            @Param("path") String path,
-            @Param("muscleId") Integer muscleId
-    );
-
-    @Modifying
-    @Query("UPDATE Exercise SET exercise_image = :exerciseImage WHERE exercise_id = :exerciseId")
-    void updateImage(
-            @Param("exerciseId") Integer id,
-            @Param("exerciseImage") String image
-    );
-
-    @Modifying
-    @Query("UPDATE Exercise SET exercise_path = :exercisePath WHERE exercise_id = :exerciseId")
-    void updatePath(
-            @Param("exerciseId") Integer id,
-            @Param("exercisePath") String path
-    );
-
-    @Modifying
-    @Query("UPDATE Exercise SET exercise_active = FALSE WHERE exercise_id = :exerciseId")
-    void delete(@Param("exerciseId") Integer id);
 }
